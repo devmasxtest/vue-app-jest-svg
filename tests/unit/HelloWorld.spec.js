33505e9300005bab24bd44a1f5e6ea5a6e7b317c
Miguel Savignano <migue.masx@gmail.com>
2018-12-02 22:58:34 +0100

    test suite

---
+++ tests/unit/HelloWorld.spec.js
@@ -0,0 +1,9 @@
+import { shallowMount } from '@vue/test-utils'
+import HelloWorld from '@/components/HelloWorld.vue'
+describe('HelloWorld', () => {
+  it('renders props.msg when passed', () => {
+    const wrapper = shallowMount(HelloWorld)
+    expect(wrapper).toMatchSnapshot()
+  })
+
+})
