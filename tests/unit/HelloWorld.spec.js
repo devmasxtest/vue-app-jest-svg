c3cac863cc0a56dd69254cd4d8d0fab75ad12fb8
Miguel Savignano <migue.masx@gmail.com>
2018-12-04 01:28:00 +0100

    test works

--- tests/unit/HelloWorld.spec.js
+++ tests/unit/HelloWorld.spec.js
@@ -1,8 +1,9 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import { shallowMount } from '@vue/test-utils';
+import HelloWorld from '@/components/HelloWorld.vue';
+
 describe('HelloWorld', () => {
   it('renders props.msg when passed', () => {
-    const wrapper = shallowMount(HelloWorld)
+    const wrapper = shallowMount(HelloWorld, { propsData: { message: 'Hello world!' }})
     expect(wrapper).toMatchSnapshot()
   })
 
