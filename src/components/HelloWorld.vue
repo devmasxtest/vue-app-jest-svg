a9b0f1109f48c44c608c0f901fdd2384a0fb4666
Miguel Savignano <migue.masx@gmail.com>
2018-12-04 02:07:29 +0100

    example with svgs

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,29 @@
 <template>
-  <h1>{{message}}</h1>
+  <div>
+    <h1>{{message}}</h1>
+    <figure>
+      <img src="../assets/nodejs_logo.svg">
+    </figure>
+    <NodeLogo/>
+  </div>
 </template>
 <script>
+import NodeLogo from "./NodeLogo.vue";
+import { setTimeout, setInterval } from "timers";
+const genColor = () => {
+  const colors = ["#233cccaa", "#cc239daa", "#b5971faa", "#b11515aa"];
+  return colors[Math.floor(Math.random() * 3 + 0)];
+};
+
 export default {
-  props: ["message"]
+  props: ["message"],
+  components: {
+    NodeLogo
+  },
+  mounted() {
+    setInterval(() => {
+      document.querySelector("#svg-js-logo").style.fill = genColor();
+    }, 1000);
+  }
 };
 </script>
