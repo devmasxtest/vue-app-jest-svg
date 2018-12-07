12c6809817a344642ab4da5eb2bb24fd516c0095
Miguel Savignano <migue.masx@gmail.com>
2018-12-07 17:29:27 +0100

    delete exaple svg

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,25 +7,9 @@
     <NodeLogo/>
   </div>
 </template>
-<script>
-import NodeLogo from "./NodeLogo.vue";
-import { setTimeout, setInterval } from "timers";
-const genColor = () => {
-  const colors = ["#233cccaa", "#cc239daa", "#b5971faa", "#b11515aa"];
-  return colors[Math.floor(Math.random() * 3 + 0)];
-};
 
+<script>
 export default {
-  props: ["message"],
-  components: {
-    NodeLogo
-  },
-  mounted() {
-    setInterval(() => {
-      if (process.env.NODE_ENV != "test") {
-        document.querySelector("#svg-js-logo").style.fill = genColor();
-      }
-    }, 1000);
-  }
+  props: ["message"]
 };
 </script>
